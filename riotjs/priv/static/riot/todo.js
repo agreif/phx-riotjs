267f4762833802872179c6e7ff9271fdc7b5443a
Alex Greif <alex.greif@gmail.com>
2022-09-25 16:56:43 +0200

    todo tag

--- riotjs/priv/static/riot/todo.js
+++ riotjs/priv/static/riot/todo.js
@@ -1,34 +1,114 @@
 var todo = {
   css: null,
   exports: {
-    state: {
-      message: 'text defined in statr'
+    onBeforeMount(props, state) {
+      // initial state
+      this.state = {
+        items: props.items,
+        text: ''
+      };
     },
 
-    get stateMessage() {
-      return this.state.message;
+    edit(e) {
+      // update only the text state
+      this.update({
+        text: e.target.value
+      });
     },
 
-    get propsMessage() {
-      return this.props.message;
+    add(e) {
+      e.preventDefault();
+
+      if (this.state.text) {
+        this.update({
+          items: [...this.state.items, // add a new item
+          {
+            title: this.state.text
+          }],
+          text: ''
+        });
+      }
+    },
+
+    toggle(item) {
+      item.done = !item.done; // trigger a component update
+
+      this.update();
     }
 
   },
-  template: (template, expressionTypes, bindingTypes, getComponent) => template('<h1 expr2="expr2"> </h1><p expr3="expr3"> </p>', [{
+  template: (template, expressionTypes, bindingTypes, getComponent) => template('<h3 expr2="expr2"> </h3><ul><li expr3="expr3"></li></ul><form expr6="expr6"><input expr7="expr7"/><button expr8="expr8"> </button></form>', [{
     redundantAttribute: 'expr2',
     selector: '[expr2]',
     expressions: [{
       type: expressionTypes.TEXT,
       childNodeIndex: 0,
-      evaluate: _scope => _scope.stateMessage
+      evaluate: _scope => _scope.props.title
     }]
   }, {
+    type: bindingTypes.EACH,
+    getKey: null,
+    condition: null,
+    template: template('<label expr4="expr4"><input expr5="expr5" type="checkbox"/> </label>', [{
+      redundantAttribute: 'expr4',
+      selector: '[expr4]',
+      expressions: [{
+        type: expressionTypes.TEXT,
+        childNodeIndex: 1,
+        evaluate: _scope => [_scope.item.title].join('')
+      }, {
+        type: expressionTypes.ATTRIBUTE,
+        name: 'class',
+        evaluate: _scope => _scope.item.done ? 'completed' : null
+      }]
+    }, {
+      redundantAttribute: 'expr5',
+      selector: '[expr5]',
+      expressions: [{
+        type: expressionTypes.ATTRIBUTE,
+        name: 'checked',
+        evaluate: _scope => _scope.item.done
+      }, {
+        type: expressionTypes.EVENT,
+        name: 'onclick',
+        evaluate: _scope => () => _scope.toggle(_scope.item)
+      }]
+    }]),
     redundantAttribute: 'expr3',
     selector: '[expr3]',
+    itemName: 'item',
+    indexName: null,
+    evaluate: _scope => _scope.state.items
+  }, {
+    redundantAttribute: 'expr6',
+    selector: '[expr6]',
+    expressions: [{
+      type: expressionTypes.EVENT,
+      name: 'onsubmit',
+      evaluate: _scope => _scope.add
+    }]
+  }, {
+    redundantAttribute: 'expr7',
+    selector: '[expr7]',
+    expressions: [{
+      type: expressionTypes.EVENT,
+      name: 'onkeyup',
+      evaluate: _scope => _scope.edit
+    }, {
+      type: expressionTypes.VALUE,
+      evaluate: _scope => _scope.state.text
+    }]
+  }, {
+    redundantAttribute: 'expr8',
+    selector: '[expr8]',
     expressions: [{
       type: expressionTypes.TEXT,
       childNodeIndex: 0,
-      evaluate: _scope => _scope.propsMessage
+      evaluate: _scope => ['Add #', _scope.state.items.length + 1].join('')
+    }, {
+      type: expressionTypes.ATTRIBUTE,
+      name: 'disabled',
+      evaluate: _scope => !_scope.state.text
     }]
   }]),
   name: 'todo'
